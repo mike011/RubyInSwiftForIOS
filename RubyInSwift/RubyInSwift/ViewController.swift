0ad0c29d3b837c64ae787d9b1fc60aa2891f5fc9
Michael Charland <mike_ch_1@hotmail.com>
2019-07-11 13:19:55 -0400

    First commit

---
+++ RubyInSwift/RubyInSwift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RubyInSwift
+//
+//  Created by Michael Charland on 2019-07-11.
+//  Copyright © 2019 Michael Charland. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
